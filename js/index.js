603a918f70f8fbb70dc103b1f72b326afbb7ca97
NadiaZaheer <116955943+NadiaZaheer@users.noreply.github.com>
2023-04-17 17:14:34 -0400

    comments added

--- js/index.js
+++ js/index.js
@@ -6,14 +6,15 @@ const workSection=document.getElementById('id-myWork');
 scrollTomyWorkButton.addEventListener('click',()=>{workSection.scrollIntoView({behavior:'smooth'});
 });
 
-const contactLink = document.querySelector('a[href="#contact"]');
-const contactForm = document.querySelector("form");
+const contactLink = document.querySelector('a[href="#contact"]'); //select the contact link
+const contactForm = document.querySelector("form");//select the contact form
 contactLink.addEventListener("click",function(event){
-  event.preventDefault();
-  const contactFormPosition=contactForm.getBoundingClientRect().top;
+  event.preventDefault(); //preven the default action of the link
+  const contactFormPosition=contactForm.getBoundingClientRect().top;//get the position of the contact form relative to the view port
   window.scrollTo({
 
-    top:contactFormPosition,behavior:"smooth"
+    top:contactFormPosition,//scroll to the position of the contact form
+    behavior:"smooth"//animate to scroll
 
   });
 });
