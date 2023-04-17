15ec94cd1a0a5a9c091f92e5759332219b6f9b96
NadiaZaheer <116955943+NadiaZaheer@users.noreply.github.com>
2023-04-17 16:38:59 -0400

    js added for contact tab and view my work button

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+
+
+const scrollTomyWorkButton=document.getElementById('scroll-to-myWork');
+const workSection=document.getElementById('id-myWork');
+
+scrollTomyWorkButton.addEventListener('click',()=>{workSection.scrollIntoView({behavior:'smooth'});
+});
+
+const contactLink = document.querySelector('a[href="#contact"]');
+const contactForm = document.querySelector("form");
+contactLink.addEventListener("click",function(event){
+  event.preventDefault();
+  const contactFormPosition=contactForm.getBoundingClientRect().top;
+  window.scrollTo({
+
+    top:contactFormPosition,behavior:"smooth"
+
+  });
+});
