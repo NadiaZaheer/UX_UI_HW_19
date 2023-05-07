3e66350327290df32495a5e6cde5a4ed61ed770e
NadiaZaheer <116955943+NadiaZaheer@users.noreply.github.com>
2023-05-07 08:03:49 -0400

    Portfolio adjustments

--- js/index.js
+++ js/index.js
@@ -1,15 +1,10 @@
 
 
-const scrollTomyWorkButton=document.getElementById('scroll-to-myWork');
-const workSection=document.getElementById('id-myWork');
-
-scrollTomyWorkButton.addEventListener('click',()=>{workSection.scrollIntoView({behavior:'smooth'});
-});
 
 const contactLink = document.querySelector('a[href="#contact"]'); //select the contact link
 const contactForm = document.querySelector("form");//select the contact form
 contactLink.addEventListener("click",function(event){
-  event.preventDefault(); //preven the default action of the link
+  event.preventDefault(); //prevent the default action of the link
   const contactFormPosition=contactForm.getBoundingClientRect().top;//get the position of the contact form relative to the view port
   window.scrollTo({
 
@@ -18,3 +13,13 @@ contactLink.addEventListener("click",function(event){
 
   });
 });
+
+
+$('.navbar-toggler').click(function() {
+  $('.overlay').toggle();
+});
+
+
+
+
+
